541a71c9ac62fc7ff2b8fcbb95ecf46d694df616
Victor Marquez <marquezvictor1599@gmail.com>
2019-01-04 12:01:30 -0500

    done

---
+++ src/main/java/org/launchcode/controllers/CategoryControllers.java
@@ -0,0 +1,49 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Category;
+import org.launchcode.models.data.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("category")
+
+public class CategoryControllers {
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model){
+        model.addAttribute("title", "Category Index");
+        model.addAttribute("categories", categoryDao.findAll());
+
+        return "category/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model){
+        model.addAttribute("title", "Add Category");
+        model.addAttribute(new Category());
+
+        return "category/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(@ModelAttribute @Valid Category category, Errors errors, Model model){
+        if (errors.hasErrors()){
+            model.addAttribute("title", "Add Categoty");
+            return "category/add";
+        }
+        categoryDao.save(category);
+        return "redirect:";
+    }
+}
+
+
