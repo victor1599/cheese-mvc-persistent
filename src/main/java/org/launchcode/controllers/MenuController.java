63c5c5436046c953da255115efffed0d734921c0
Victor Marquez <marquezvictor1599@gmail.com>
2019-01-06 19:42:05 -0500

    done

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,81 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Cheese;
+import org.launchcode.models.Menu;
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.launchcode.models.forms.AddMenuItemForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping(value = "menu")
+public class MenuController {
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model){
+        model.addAttribute("title", "Menus");
+        model.addAttribute("menus", menuDao.findAll());
+
+        return "menu/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddMenuForm(Model model){
+        model.addAttribute("title", "Add Menu");
+        model.addAttribute(new Menu());
+
+        return "menu/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddMenuForm(@ModelAttribute @Valid Menu menu, Errors errors, Model model){
+        if (errors.hasErrors()){
+            model.addAttribute("title", "Add Menu");
+            return "menu/add";
+        }
+
+        menuDao.save(menu);
+        return "redirect:view/" + menu.getId();
+    }
+
+    @RequestMapping(value = "view/{menuId}")
+    public String viewMenu(@PathVariable("menuId") int menuId, Model model){
+        Menu theMenu = menuDao.findOne(menuId);
+        model.addAttribute("title", theMenu.getName() );
+        model.addAttribute("menu", theMenu);
+
+        return "menu/viewOne";
+    }
+
+    @RequestMapping(value = "add-item/{menuId}")
+    public String addItem(@PathVariable("menuId") int menuId, Model model){
+        Menu theMenu = menuDao.findOne(menuId);
+        AddMenuItemForm form = new AddMenuItemForm(theMenu, cheeseDao.findAll());
+
+        model.addAttribute("form", form);
+        model.addAttribute("title", "Add item to " + theMenu.getName());
+        return "menu/add-item";
+    }
+
+    @RequestMapping(value = "add-item", method = RequestMethod.POST)
+    public String addItem(@ModelAttribute @Valid AddMenuItemForm form, Errors errors, Model model){
+        Menu theMenu = menuDao.findOne(form.getMenuId());
+        Cheese theCheese = cheeseDao.findOne(form.getCheeseId());
+        theMenu.addItem(theCheese);
+
+        menuDao.save(theMenu);
+        return "redirect:/menu/view/" + theMenu.getId(); //plus whatever the menuId is
+    }
+
+}
--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,10 +1,10 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 
@@ -16,5 +16,24 @@ public class Category {
     @NotNull
     @Size(min=3, max=15)
     private String name;
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
+
+    public Category(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
 
 }
